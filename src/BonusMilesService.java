cdb671fa6ecabd86d4d6e39a2e74f20661f0c00a
Элина Хафизова <elina6hafizova@yandex.ru>
2022-05-28 12:10:44 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,15 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {     //стоимость билета
+        int miles;                       //возвращает расчитанное количество миль
+
+        if (cost > 20) {
+          miles = cost/20;
+
+        } else{
+            miles = 0;
+        }
+        return miles;
+    }
+
+}
